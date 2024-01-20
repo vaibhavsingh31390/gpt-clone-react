9ff128aa0dd0943a5b26cb95c64f36a58ea4858b
Vaibhav Singh <vaibhavsingh31390@gmail.com>
2024-01-21 04:49:10 +0530

    [UPDATE] added vercel endpint

--- src/Utils/Routes.js
+++ src/Utils/Routes.js
@@ -1,5 +1,5 @@
 const routes = {
-  host: `https://gpt-backend-one.vercel.app/`,
+  host: `https://gpt-backend-one.vercel.app`,
   // host: `http://127.0.0.1:6050`,
   login: `/api/v1/users/login`,
   register: `/api/v1/users/register`,
