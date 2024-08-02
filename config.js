5c37c215646251c8b432096200bc54265770fbc3
Vaibhav Singh <vaibhavsingh31390@gmail.com>
2024-08-03 02:19:28 +0530

    [UPDATE] changess

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const API_HOST = "http://65.1.132.5:8005";
-export const API_HOST_DEV = "http://127.0.0.1:6050";
+export const API_HOST_DEV = "http://65.1.132.5:8005";
 export const APP_ENV = "PROD";
